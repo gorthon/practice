f4eb92dfa49487f2a90c734a5380afb6b374b120
dobo <dobo@bunito>
2015-11-11 17:39:54 -0600

    ch 4 save 2

--- oglsb/ch4/dobogl.c
+++ oglsb/ch4/dobogl.c
@@ -22,14 +22,21 @@ void dglCopy4f(GLfloat * from, GLfloat * to) {
 	to[3] = from[3];
 }
 GLfloat* dglMultiply33f3f(GLfloat * A, GLfloat * v) {
-	GLfloat* ret = {
-		A[0]*v[0]+A[3]*v[1]+A[6]*v[2],
-		A[1]*v[0]+A[4]*v[1]+A[7]*v[2],
-		A[2]*v[0]+A[5]*v[1]+A[8]*v[2]
-	};
+	GLfloat* ret = (GLfloat *)malloc((sizeof GLfloat) * 3);
+	ret[0] = A[0]*v[0]+A[3]*v[1]+A[6]*v[2];
+	ret[1] = A[1]*v[0]+A[4]*v[1]+A[7]*v[2];
+	ret[2] = A[2]*v[0]+A[5]*v[1]+A[8]*v[2];
 	return ret;
 }
 GLfloat* dglMultiply44f4f(GLfloat * A, GLfloat * v) {
+	GLfloat* ret = (GLfloat *)malloc((sizeof GLfloat) * 4);
+	ret[0] = A[0]*v[0]+A[4]*v[1]+A[8]*v[2];
+	ret[1] = A[1]*v[0]+A[5]*v[1]+A[9]*v[2];
+	ret[2] = A[2]*v[0]+A[6]*v[1]+A[10]*v[2];
+	ret[3] = A[3]*v[0]+A[7]*v[1]+A[11]*v[2];
+	return ret;
+}
+GLfloat* dglMultiply33f33f(GLfloat * A, GLfloat * B) {
 	GLfloat* ret = {
 		A[0]*v[0]+A[3]*v[1]+A[6]*v[2],
 		A[1]*v[0]+A[4]*v[1]+A[7]*v[2],
@@ -37,15 +44,7 @@ GLfloat* dglMultiply44f4f(GLfloat * A, GLfloat * v) {
 	};
 	return ret;
 }
-GLfloat* dglMultiply33f33f(GLfloat * A, GLfloat * v) {
-	GLfloat* ret = {
-		A[0]*v[0]+A[3]*v[1]+A[6]*v[2],
-		A[1]*v[0]+A[4]*v[1]+A[7]*v[2],
-		A[2]*v[0]+A[5]*v[1]+A[8]*v[2]
-	};
-	return ret;
-}
-GLfloat* dglMultiply44f44f(GLfloat * A, GLfloat * v) {
+GLfloat* dglMultiply44f44f(GLfloat * A, GLfloat * B) {
 	GLfloat* ret = {
 		A[0]*v[0]+A[3]*v[1]+A[6]*v[2],
 		A[1]*v[0]+A[4]*v[1]+A[7]*v[2],
